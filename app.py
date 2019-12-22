931b5835f0c8215b3f9e495a9bfd584faa7d1cff
Alex Drinkwater <aldrinkwater@hotmail.com>
2019-12-22 15:28:12 +0000

    Updated for the jargon buster

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
-app.config["MONGO_DBNAME"] = 'book_review'
-app.config['MONGO_URI'] = 'mongodb+srv://root:rootUser@myfirstcluster-lrum9.mongodb.net/book_review?retryWrites=true&w=majority'
+app.config["MONGO_DBNAME"] = 'jargon_buster'
+app.config['MONGO_URI'] = 'mongodb+srv://root:rootUser@myfirstcluster-lrum9.mongodb.net/jargon_buster?retryWrites=true&w=majority'
 
 mongo = PyMongo(app)
 
@@ -16,84 +16,89 @@ mongo = PyMongo(app)
 
 
 @app.route('/')
-@app.route('/get_book')
-def get_book():
-    return render_template("books.html", books=mongo.db.books.find())
+@app.route('/get_jargon')
+def get_jargon():
+    return render_template("jargon.html", jargons=mongo.db.jargon.find())
 
 
-# redirct function to the page where users can add a book.
-@app.route('/add_book')
-def add_book():
-    return render_template('addbook.html')
+@app.route('/add_jargon')
+def add_jargon():
+    return render_template('addjargon.html',
+                           categories=mongo.db.categories.find())
 
 
-@app.route('/add_review')
-def add_review():
-    return render_template('addreview.html')
+@app.route('/insert_jargon', methods=['POST'])
+def insert_jargon():
+    jargon = mongo.db.jargon
+    jargon.insert_one(request.form.to_dict())
+    return redirect(url_for('get_jargon'))
 
 
-# function to add the book to the database.
-@app.route('/insert_book', methods=['POST'])
-def insert_book():
-    books = mongo.db.books
-    books.insert_one(request.form.to_dict())
-    return redirect(url_for('get_book'))
+@app.route('/edit_jargon/<jargon_id>')
+def edit_jargon(jargon_id):
+    the_jargon = mongo.db.jargon.find_one({"id": ObjectId(jargon_id)})
+    all_categories = mongo.db.categories.find()
+    return render_template('editjargon.html', jargon=the_jargon,
+                           categories=all_categories)
 
 
-@app.route('/insert_review', methods=['POST'])
-def insert_review():
-    review = mongo.db.users
-    review.insert_one(request.form.to_dict())
-    return redirect(url_for('view_book'))
+@app.route('/update_jargon/<jargon_id>', methods=['POST'])
+def update_jargon(jargon_id):
+    jargon = mongo.db.jargon
+    jargon.update({'_id': ObjectId(jargon_id)},
+                  {
+            'jargon_name': request.form.get('jargon_name'),
+            'category_name': request.form.get('category_name'),
+            'description': request.form.get('description')
+        })
 
 
-# This function redirects to a view page with more information on the book that
-# was clicked on.
-@app.route('/view_book/<book_id>')
-def view_book(book_id):
-    the_book = mongo.db.books.find_one({"_id": ObjectId(book_id)})
-    return render_template('viewbook.html', book=the_book)
+@app.route('/delete_jargon/<jargon_id>')
+def delete_jargon(jargon_id):
+    mongo.dg.remove({'_id': ObjectId(jargon_id)})
+    return redirect(url_for('get_jargon'))
 
 
-# This function shows all of the books on the viewbook.html page, when the book
-# is clicked on from the home page.
-@app.route('/show_book/<book_id>')
-def show_book(book_id):
-    books = mongo.db.books
-    books.find_one({'_id': ObjectId(book_id)}, {
-        'book_name': request.form.get('book_name')
-         })
+@app.route('/get_categories')
+def get_categories():
+    return render_template('categories.html',
+                           categories=mongo.db.categories.find())
 
 
-# function to redirect to the page to edit book details.
-@app.route('/edit_book/<book_id>')
-def edit_book(book_id):
-    the_book = mongo.db.books.find_one({"_id": ObjectId(book_id)})
-    return render_template('editbook.html', book=the_book)
+@app.route('/delete_category/<category_id>')
+def delete_category(category_id):
+    mongo.db.categories.remove({'_id': ObjectId(category_id)})
+    return redirect(url_for('get_categories'))
 
 
-@app.route('/update_book/<book_id>', methods=["POST"])
-def update_book(book_id):
-    books = mongo.db.books
-    books.update({'_id': ObjectId(book_id)}, {
-        'book_name': request.form.get('book_name'),
-        'author_name': request.form.get('author_name'),
-        'cover_link': request.form.get('cover_link'),
-        'link': request.form.get('link'),
-        'Genre': request.form.get('Genre'),
-        'published_date': request.form.get('published_date'),
-        
-    })
-    return redirect(url_for('get_book'))
+@app.route('/edit_category/<category_id>')
+def edit_category(category_id):
+    return render_template('editcategory.html',
+                           category=mongo.db.categories.find_one
+                           ({'_id': ObjectId()}))
 
-# Deletes a book
-@app.route('/delete_book/<book_id>')
-def delete_book(book_id):
-    mongo.db.books.remove({'_id': ObjectId(book_id)})
-    return redirect(url_for('get_book'))
+
+@app.route('/update_category/<category_id>', methods=['POST'])
+def update_category(category_id):
+    mongo.db.categories.update(
+        {'_id': ObjectId(category_id)},
+        {'category_name': request.form.get('category_name')})
+    return redirect(url_for('get_categories'))
+
+
+@app.route('/insert_category', methods=['POST'])
+def insert_category():
+    category_doc = {'category_name': request.form.get('category_name')}
+    mongo.db.categories.insert_one(category_doc)
+    return redirect(url_for('get_categories'))
+
+
+@app.route('/add_category')
+def add_category():
+    return render_template('addcategory.html')
 
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')),
+            port=int(os.environ.get('PORT', '3000')),
             debug=True)
